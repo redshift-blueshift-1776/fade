2b258aa00039adee8295aa4fc26a0a798d78a66a
Forest Ho-Chen <foresth168@gmail.com>
2025-10-18 12:18:10 -0400

    Sidewalks

--- Assets/Scripts/City_Generator.cs
+++ Assets/Scripts/City_Generator.cs
@@ -12,7 +12,8 @@ public class City_Generator : MonoBehaviour
     [SerializeField] public int z_dimension; // Should be an odd number
     [SerializeField] public float x_length;
     [SerializeField] public float z_length;
-    [SerializeField] public float sidewalk_length;
+    [SerializeField] public float sidewalk_width;
+    [SerializeField] public float road_width;
 
     [Header("Prefabs")]
     [SerializeField] public GameObject sidewalk;
@@ -41,6 +42,13 @@ public class City_Generator : MonoBehaviour
 
                 int height = UnityEngine.Random.Range(1, 6) * 15;
                 newBuilding.transform.localScale = new Vector3(1, (float) height, 1);
+
+                GameObject newSidewalk = Instantiate(sidewalk);
+                globalX = (i - (x_dimension - 1) / 2) * x_length;
+                globalZ = (j - (z_dimension - 1) / 2) * z_length;
+
+                newSidewalk.transform.position = new Vector3(globalX, 0, globalZ);
+                newSidewalk.transform.localScale = new Vector3(x_length - road_width, 1, z_length - road_width);
             }
         }
     }

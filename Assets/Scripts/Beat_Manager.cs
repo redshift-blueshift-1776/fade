ff8622727361f7d3773756263df3a2e3104386d7
Forest Ho-Chen <foresth168@gmail.com>
2025-10-18 12:40:46 -0400

    Streetlight code

---
+++ Assets/Scripts/Beat_Manager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatManager : MonoBehaviour {
+    public static BeatManager Instance;
+    
+    public AudioSource audioSource;
+    public double secondsPerBeat;
+
+    [SerializeField] public float tempo = 120f; // Tempo of the song used
+
+    public double StartDspTime { get; private set; }
+
+    void Awake() {
+        Instance = this;
+    }
+
+    void Start() {
+        // Sync when audio starts
+        StartDspTime = AudioSettings.dspTime;
+        secondsPerBeat = 60f / tempo;
+    }
+
+    public double GetSecondsPerBeat() {
+        return secondsPerBeat;
+    }
+
+    public double GetNextBeatTime() {
+        double timeSinceStart = AudioSettings.dspTime - StartDspTime;
+        int beatsPassed = Mathf.FloorToInt((float)(timeSinceStart / secondsPerBeat));
+        return StartDspTime + (beatsPassed + 1) * secondsPerBeat;
+    }
+
+    public int GetCurrentBeatNumber() {
+        return Mathf.FloorToInt((float)((AudioSettings.dspTime - StartDspTime) / secondsPerBeat));
+    }
+}

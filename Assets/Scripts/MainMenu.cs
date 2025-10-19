017f92552dd2c9a2735f357a3764f14d8e7c62eb
justclaner <justclaner@gmail.com>
2025-10-19 13:34:40 -0400

    changed menu text to anchor position

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,10 +10,17 @@ public class MainMenu : MonoBehaviour
     [SerializeField] public GameObject textObject;
     [SerializeField] public TMP_Text text;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private RectTransform textRectTransform;
+    private Vector2 startAnchoredPos;
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        textRectTransform = textObject.GetComponent<RectTransform>();
+        startAnchoredPos = textRectTransform.anchoredPosition;
+
         StartCoroutine(DoTextStuff());
     }
 
@@ -28,9 +35,8 @@ public class MainMenu : MonoBehaviour
 
     public IEnumerator DoTextStuff() {
         float elapsed = 0f;
-        Vector3 startPos = textObject.transform.position;
         while (true) {
-            textObject.transform.position = startPos + new Vector3(0, 20 * Mathf.Sin(elapsed), 0);
+            textRectTransform.anchoredPosition = startAnchoredPos + new Vector2(0, 20f * Mathf.Sin(elapsed));
             text.color = new Color(text.color.r, text.color.g, text.color.b, 0.9f + 0.1f * Mathf.Cos(elapsed));
             elapsed += Time.deltaTime;
             yield return null;

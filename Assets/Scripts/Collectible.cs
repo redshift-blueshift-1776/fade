de35e1de32f810ed519c0b3a28c0b69982d65390
justclaner <justclaner@gmail.com>
2025-10-19 01:22:10 -0400

    added sound to collectibles

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -8,10 +8,16 @@ public class Collectible : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
     [SerializeField] private AudioManager audioManager;
+
+    private AudioSource audioSource;
+    private float clipLength;
+    private GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        clipLength = audioSource.clip.length;
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
@@ -30,5 +36,24 @@ public class Collectible : MonoBehaviour
         }
     }
     
-    
+    private IEnumerator handleSound()
+    {
+        float t = 0;
+        float cooldownTimer;
+
+        while (true)
+        {
+            float distance = (player.transform.position - transform.position).magnitude;
+            cooldownTimer = Mathf.Clamp(0.025f * distance, clipLength, 5f);
+            if (t < cooldownTimer)
+            {
+                t += Time.deltaTime;
+            } else
+            {
+                audioSource.Play();
+                t = 0;
+            }
+            yield return null;
+        }
+    }
 }
